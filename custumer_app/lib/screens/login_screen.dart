db171e47e6d519d7171fc344829e54a9b1d31b31
PrimeGestao <67592458+Felip-Oliv@users.noreply.github.com>
2023-08-15 15:47:04 -0300

    Update login_screen.dar
    Adicionando logo go cheff

--- custumer_app/lib/screens/login_screen.dart
+++ custumer_app/lib/screens/login_screen.dart
@@ -2,47 +2,49 @@ import 'package:flutter/material.dart';
 import 'package:custumer_app/screens/login_screen.dart';
 import 'package:custumer_app/screens/home.dart';
 
-class LoginScreen extends StatefulWidget{
+class LoginScreen extends StatefulWidget {
   @override
   _LoginScreenState createState() => _LoginScreenState();
-
 }
-class _LoginScreenState extends State <LoginScreen>{
+
+class _LoginScreenState extends State<LoginScreen> {
   Color topColor = Colors.orange;
-  Color bottomColor = Color.fromRGBO(
-      214,
-      134,
-      60,
-      0.9
-  );
+  Color bottomColor = Colors.white;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(
+          decoration: BoxDecoration(
             gradient: LinearGradient(
                 begin: Alignment.topCenter,
                 end: Alignment.bottomCenter,
-                colors: [
-                  topColor,
-                  bottomColor
-                ]),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-
-          children: [Text("Entrar", textAlign: TextAlign.center,
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-
+                colors: [topColor, bottomColor]),
           ),
-          )],
-        )
-     ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Image.asset(
+                "assets/logo_fundo_transparente.png",
+                height: 125,
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  bottom: 50,
+                ),
+              ),
+              Text(
+                "Entrar",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+            ],
+          )),
     );
   }
 }
