3af927985c2dfb624a7a87336bf916a225f8bb71
Luiz Felipe A. Oliveira <67592458+Felip-Oliv@users.noreply.github.com>
2023-08-27 15:24:38 -0300

    Update custom_colors.dart
    Adicionando cores dos botões.

--- custumer_app/lib/values/custom_colors.dart
+++ custumer_app/lib/values/custom_colors.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
 
 class CustomColors{
+  Color _activePrimaryButton = Color.fromARGB(255, 63, 81, 181);
+  Color _activeSecondaryButton = Color.fromARGB(255, 200, 200, 200);
 
+  Color getActivePrimaryButtonColor() {
+
+    return _activePrimaryButton;
+  }
+
+  Color getActiveSecondaryButtonColor() {
+    return _activeSecondaryButton;
+
+  }
 }
+
